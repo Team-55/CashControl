302f2981edc824cdf09fa53047f5fce7fe0ece0f
Maxim V <m4k51m15k4m@gmail.com>
2024-05-09 15:19:29 +0300

    add  cash/views.py

--- backend/cash/views.py
+++ backend/cash/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Plan
+from .serializers import PlanSerializer
+
+
+class PlanViewSet(viewsets.ModelViewSet):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+
+    def get_queryset(self):
+        return Plan.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
